41419ba61f51527da1e250602741b802e9f95d72
Alexander Gottwald <alexander.gottwald@gmail.com>
2020-10-26 15:30:15 +0100

    shared_with_groups is an object
    GitLabProject.shared_with_groups is an object, not a string

---
+++ src/main/java/cd/go/authorization/gitlab/client/models/SharedWithGroup.java
@@ -0,0 +1,46 @@
+
+package cd.go.authorization.gitlab.client.models;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class SharedWithGroup {
+    @Expose
+    @SerializedName("group_id")
+    private long groupId;
+    @Expose
+    @SerializedName("group_name")
+    private String groupName;
+    @Expose
+    @SerializedName("group_full_path")
+    private String groupFullPath;
+    @Expose
+    @SerializedName("access_level")
+    private AccessLevel accessLevel;
+    @Expose
+    @SerializedName("expires_at")
+    private String expiresAt;
+
+    SharedWithGroup() {
+    }
+
+    public long getGroupId() {
+        return groupId;
+    }
+
+    public String getGroupName() {
+        return groupName;
+    }
+    
+    public String getGroupFullPath() {
+        return groupFullPath;
+    }
+
+    public AccessLevel getAccessLevel() {
+        return accessLevel;
+    }
+
+    public String getExpiresAt() {
+        return expiresAt;
+    }
+}
