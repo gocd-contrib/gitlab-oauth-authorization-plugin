0ec150c297c945cb01e368c10412cf294877b141
Alexander Gottwald <alexander.gottwald@gmail.com>
2020-10-26 15:40:48 +0100

    fix missing include

--- src/main/java/cd/go/authorization/gitlab/client/GitLabClient.java
+++ src/main/java/cd/go/authorization/gitlab/client/GitLabClient.java
@@ -26,6 +26,7 @@ import cd.go.authorization.gitlab.models.TokenInfo;
 import okhttp3.*;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
